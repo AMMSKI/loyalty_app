782c8ef35933407af18f20f1fc58462c8658be43
Elise Brown <elisemarie88@gmail.com>
2021-10-14 14:30:56 -0600

    styling admin

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { Header } from 'semantic-ui-react';
+import React, { useContext } from 'react';
+import { Card, CardGroup, Grid, GridColumn, GridRow, Header, Segment } from 'semantic-ui-react';
+import Avatar from 'react-avatar';
+import { AuthContext } from '../providers/AuthProvider';
+import { Col } from 'react-bootstrap';
 
 function Admin(props) {
+  const { user } = useContext(AuthContext)
+
   return (
-    <div>
-      <Header>Admin</Header>
+    <div style={{padding:"30px"}}>
+      <div className="jumbotron text-center">
+      <Avatar size="100" style={{border:"solid 1px lightgray"}} round src={user.image}/> 
+      <h1>{user.name}</h1>
+      <p>{user.account_type}</p>
+      </div>
+      <Grid textAlign='center' celled columns='equal'>
+        <GridRow>
+        <Grid.Column>
+          Cards
+        </Grid.Column>
+        <Grid.Column>
+          Data
+        </Grid.Column>
+        <Grid.Column>
+          Data
+        </Grid.Column>
+      </GridRow>
+      <GridRow style={{height:"250px"}}>
+        <GridColumn>Need help</GridColumn>
+      </GridRow>
+    </Grid>
     </div>
   );
 }
 
 export default Admin;
+
