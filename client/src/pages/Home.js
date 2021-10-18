54bb9870ca7567941e6f3243250e5909b353d3ad
Elise Brown <elisemarie88@gmail.com>
2021-10-18 06:41:04 -0600

    home styled

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Image } from 'semantic-ui-react'
+import logo from "../burgerlogo.png"
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <div className="landing">
+      <div className="content">
+        <div className="logo">
+          <Image src={logo} size="small" verticalAlign="middle"/>
+        </div>
+          <hr className="rounded"/>
+        <div className="tagline">
+          WELCOME
+        </div>
+      </div>
+      </div>
   )
 }
+
+
 
 export default Home
