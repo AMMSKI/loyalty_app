40845239c259222fae86f27cbfd98877145e5455
Kelsey Nicholes <bethenerd.dev@gmail.com>
2021-10-20 15:06:45 -0600

    image

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,9 +4,9 @@ import { Card } from "semantic-ui-react";
 const About = () => {
   
   const description = [
-    "dsfgdfs",
-    "dfsj",
-    "hdfsjk",
+    "Fall 2021 Graduate. Looking forward to the new adventure of being a web developer!",
+    "Love to code, play golf and hang out with my family.",
+    "Excited to keep learning and growing web development and plans to be a front end developer.",
     "hkjfds",
     "hkjfds"
   ]
@@ -20,6 +20,7 @@ const About = () => {
       <Card>
         <Card.Content header='Elise' />
         <Card.Content description={description[0]} />
+        <Image src="https://files.slack.com/files-pri/T04QL6Z16-F02JC4YB42E/image_from_i" />
         <Card.Content extra>
         </Card.Content>
       </Card>,
@@ -27,6 +28,7 @@ const About = () => {
       <Card>
       <Card.Content header='Alan' />
       <Card.Content description={description[1]} />
+      <Image src= "https://files.slack.com/files-pri/T04QL6Z16-F02K5SXF124/image.png"/>
       <Card.Content extra>
       </Card.Content>
       </Card>,
@@ -34,6 +36,7 @@ const About = () => {
       <Card>
       <Card.Content header='Kelsey' />
       <Card.Content description={description[2]} />
+      <Image src=  "Kelsey_Headshot-2e.jpg"/>
       <Card.Content extra>
       </Card.Content>
       </Card>,
@@ -41,6 +44,7 @@ const About = () => {
       <Card>
       <Card.Content header='Rafael' />
       <Card.Content description={description[3]} />
+      <Image src= "https://files.slack.com/files-pri/T04QL6Z16-F02JJ40G2JG/74bde692-6a87-4a74-8d52-d8217035b7c3_1_105_c.jpeg" />
       <Card.Content extra>
       </Card.Content>
       </Card>,
@@ -48,6 +52,7 @@ const About = () => {
       <Card>
       <Card.Content header='Lee' />
       <Card.Content description={description[4]} />
+      <Image src= "https://files.slack.com/files-pri/T04QL6Z16-F02JCRD3DFF/me.png" />
       <Card.Content extra>
       </Card.Content>
       </Card>
