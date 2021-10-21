647d829ac97b64faf1ee646113e027258e9fb173
Kelsey Nicholes <bethenerd.dev@gmail.com>
2021-10-21 16:27:42 -0600

    style

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,65 +1,86 @@
 import React from "react";
-import { Card } from "semantic-ui-react";
+import { Card as div, Image } from "semantic-ui-react";
+
+import styled from 'styled-components';
+
+const Style = styled.div`
+  color: white;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    font-weight: bold;
+    margin-top: 20px;
+    margin-bottom: 30px;
+  }
+
+  .profile {
+    width: 300px;
+    margin-bottom: 50px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+
+    .first-name, p {
+      margin-top: 10px;
+    }
+  }
+`;
 
 const About = () => {
-  
+
   const description = [
     "Fall 2021 Graduate. Looking forward to the new adventure of being a web developer!",
     "Love to code, play golf and hang out with my family.",
-    "Excited to keep learning and growing web development and plans to be a front end developer.",
+    "Excited to keep learning and growing web development and plans to be a front end developer. Dev Point Labs Fall 2021 graduate.",
     "hkjfds",
-    "hkjfds"
-  ]
+    "Fall 2021 Graduate. I look forward to being a web developer by day, game designer by night."
+  ];
 
-
-  return(
-    <div>
-
+  return (
+    <Style>
       <h1>About the Coders!</h1>
 
-      <Card>
-        <Card.Content header='Elise' />
-        <Card.Content description={description[0]} />
-        <Image src="https://files.slack.com/files-pri/T04QL6Z16-F02JC4YB42E/image_from_i" />
-        <Card.Content extra>
-        </Card.Content>
-      </Card>,
+      <div className="profile">
+        <Image width="300" src="/images/elise.jpg" />
+        <b className="first-name">Elise</b>
+        <p>{description[0]}</p>
+      </div>
 
-      <Card>
-      <Card.Content header='Alan' />
-      <Card.Content description={description[1]} />
-      <Image src= "https://files.slack.com/files-pri/T04QL6Z16-F02K5SXF124/image.png"/>
-      <Card.Content extra>
-      </Card.Content>
-      </Card>,
+      <div className="profile">
+        <Image width="300" src="/images/alan.png" />
+        <b className="first-name">Alan</b>
+        <p>{description[1]}</p>
+      </div>
 
-      <Card>
-      <Card.Content header='Kelsey' />
-      <Card.Content description={description[2]} />
-      <Image src=  "Kelsey_Headshot-2e.jpg"/>
-      <Card.Content extra>
-      </Card.Content>
-      </Card>,
+      <div className="profile">
+        <Image width="300" src="/images/rafael.jpeg" />
+        <b className="first-name">Rafael</b>
+        <p>{description[3]}</p>
+      </div>
 
-      <Card>
-      <Card.Content header='Rafael' />
-      <Card.Content description={description[3]} />
-      <Image src= "https://files.slack.com/files-pri/T04QL6Z16-F02JJ40G2JG/74bde692-6a87-4a74-8d52-d8217035b7c3_1_105_c.jpeg" />
-      <Card.Content extra>
-      </Card.Content>
-      </Card>,
+      <div className="profile">
+        <Image width="300" src="/images/kelsey.gif" />
+        <b className="first-name">Kelsey</b>
+        <p>{description[3]}</p>
+      </div>
 
-      <Card>
-      <Card.Content header='Lee' />
-      <Card.Content description={description[4]} />
-      <Image src= "https://files.slack.com/files-pri/T04QL6Z16-F02JCRD3DFF/me.png" />
-      <Card.Content extra>
-      </Card.Content>
-      </Card>
-    </div>
+      <div className="profile">
+        <Image width="300" src="/images/lee.png" />
+        <b className="first-name">Lee</b>
+        <p>{description[4]}</p>
+      </div>
+
+      
+    </Style>
   )
 
-    }
+}
 
 
 
