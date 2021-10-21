eee6eabe4fdd5c714083836f9bc86a534fcb0b49
Elise Brown <elisemarie88@gmail.com>
2021-10-21 07:35:44 -0600

    Customer Rewards Card Styling

--- client/src/pages/CustomerRewards.js
+++ client/src/pages/CustomerRewards.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Card, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { Icon } from 'semantic-ui-react'
 import '../StyleSheets/CustomerRewards.css'
@@ -26,13 +25,11 @@ const CustomerRewards = ({ punchcardData, userpunchcard_id }) => {
     return rewards.map((r) => {
       return (
         <div>
-          <Card>
-            <Card.Body>
-              <h1>{r.name}</h1>
-              <Card.Text>
-                <p>{r.description}</p>
-              </Card.Text>
-            </Card.Body>
+          <div className="cust-rewards-cards">
+            <div>
+              <h1 className="rewards-title">{r.name}</h1>
+              <p>{r.description}</p>
+            </div>
 
             {r.cost < punchcardData.current_points ?
               <Link
@@ -40,16 +37,16 @@ const CustomerRewards = ({ punchcardData, userpunchcard_id }) => {
                 reward_id={r.id}
                 userpunchcard_id={userpunchcard_id} >
 
-                <Card.Footer>
+                <div className="cash-or-cost">
                   Cash In Reward
-                </Card.Footer>
+                </div>
               </Link> :
-              <Card.Footer>
-                Cost {r.cost} points
-              </Card.Footer>
+              <div className="cash-or-cost">
+                Cost: {r.cost} points
+              </div>
             }
 
-          </Card>
+          </div>
         </div>
       )
     })
@@ -58,10 +55,10 @@ const CustomerRewards = ({ punchcardData, userpunchcard_id }) => {
   return (
     <div className="customer-rewards-page">
       <h2
-        className="rewards-title"
+        className="rewards-header"
         onClick={() => getRewards()}>
         <Icon name="arrow alternate circle down" color="yellow" />
-          Rewards 
+          Rewards
         <Icon name="arrow alternate circle down" color="yellow" />
       </h2>
       {showRewards && renderRewards()}
