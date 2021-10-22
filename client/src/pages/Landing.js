9791fea1cdc2e360053c78d52fbeaa1bf9ad4392
Alan Mamulski <alanmamulski@gmail.com>
2021-10-22 16:07:34 -0600

    login showing when already logged in FIXED

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router';
 import { Image } from 'semantic-ui-react';
 import logo from "../images/burgerlogo.png";
+import { AuthContext } from '../providers/AuthProvider';
 import '../StyleSheets/LandingHome.css';
+import Home from './Home';
 
 
 const Landing = () => {
   const history = useHistory()
+  const { user } = useContext(AuthContext)
 
+  if(user){
+    return (
+      <Home />
+    )
+  }else{
   return (
     <div>
       <div className="landing">
@@ -28,6 +36,7 @@ const Landing = () => {
       </div>
     </div>
   )
+  }
 }
 
 export default Landing;
