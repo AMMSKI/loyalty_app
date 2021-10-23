ed16870efa1e5ff8fa41b581f9932713799a6b59
Rafael Briones <rbrionesdev@outlook.com>
2021-10-22 20:25:56 -0600

    debuged navbar had a conflict with edit user conditional = === last chunk code

--- client/src/Components/EditUser.js
+++ client/src/Components/EditUser.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { useHistory } from 'react-router'
-import { Button, Form, Grid, Header, Image, Segment } from 'semantic-ui-react'
+import { Form, Grid, Header, Segment } from 'semantic-ui-react'
 import { AuthContext } from '../providers/AuthProvider';
 import ErrorMessage from './ErrorMessage';
 import PunchcardImageUpload from './PunchcardUpload';
@@ -25,63 +25,57 @@ export default function EditUser() {
 
   return (
     <div className="edit-user-page">
-      {error && <ErrorMessage header="Could not Update" error={error} />}
-      <Grid textAlign='center' style={{ height: '54vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h1" mini textAlign='center' style={{color:"#D7272F"}}>
-            Update profile
-          </Header>
-          <Form size='large' onSubmit={handleSubmit}>
-            <Segment padded>
-              <Form.Input
-                required
-                // label="Name"
-                fluid icon='user'
-                iconPosition='left'
-                // placeholder='Name'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-              {/* <Form.Input
-                // label="Account Type"
-                fluid icon='building'
-                iconPosition='left'
-                // placeholder='Account Type'
-                value={account_type}
-                onChange={(e) => setAccount_Type(e.target.value)}
-              /> */}
-              <Form.Input
-                // label="Password"
-                required
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-                type='password'
-                // value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Form.Input
-                // label={"Confirm Password"}
-                required
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password Confirmation'
-                type='password'
-                // value={passwordConfirmation}
-                onChange={(e) => setPasswordConfirmation(e.target.value)}
-              />
-              <button
-                className="update-profile-button"
+      <div className="default-editpage">
+        {error && <ErrorMessage header="Could not Update" error={error} />}
+        <Grid textAlign='center' verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h1" mini textAlign='center' style={{ color: "#D7272F" }}>
+              Update profile
+            </Header>
+            <Form size='large' onSubmit={handleSubmit}>
+              <Segment padded>
+                <Form.Input
+                  required
+                  // label="Name"
+                  fluid icon='user'
+                  iconPosition='left'
+                  // placeholder='Name'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <Form.Input
+                  // label="Password"
+                  required
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                  // value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Form.Input
+                  // label={"Confirm Password"}
+                  required
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password Confirmation'
+                  type='password'
+                  // value={passwordConfirmation}
+                  onChange={(e) => setPasswordConfirmation(e.target.value)}
+                />
+                <button
+                  className="update-profile-button"
                 >
-                UPDATE
-              </button>
-            </Segment>
-          </Form>
-        </Grid.Column>
-      </Grid>
-      { user.account_type = "business" && <PunchcardImageUpload />}
+                  UPDATE
+                </button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+        {user.account_type === "business" && <PunchcardImageUpload />}
+      </div>
     </div>
   )
 }
